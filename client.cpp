b579ddff1f7e2a71e31363940e7c4575a1bdc8b7
tuh37195 <tuh37195@temple.edu>
2019-10-27 19:06:55 -0400

    Packets now sorted and combined

--- client.cpp
+++ client.cpp
@@ -14,6 +14,8 @@
 #define MAX_WAIT 1
 //local port for UDP stream
 #define LOCAL_PORT 8888
+//our *MODIFIED* RTP header size
+#define RTP_HEADER_SIZE 14
 
 
 #include <string>
@@ -203,17 +205,96 @@ bool session_handler(SOCKET rtsp_socket, int &request_count, string &session_id)
 	return true;	
 }
 
-void do_packet_stuff(char *buffer){
-	/* TODO: 
-	/	Sort packets by sequence number
-	/	remove rdp header from packet	
-	/	take a quick cry break
-	/	reassemble packets into file
-	*/
+//sends frame to decoder
+void frame_to_decoder(string frame){
+	FILE *file = fopen("/tmp/test.mp4", "w+");
+	std::fwrite(frame.c_str(), frame.length(), 1, file);
+	return;
 }
 
+void merge_frame(char **&frame_buffer, int packet_count){
+	size_t size = 0;
+	std::ostringstream frame;
+	//check if all packets are assembled
+	for (size_t i = 0; i < packet_count; i++){
+		
+		if (frame_buffer[i] != NULL){
+			frame << frame_buffer[i];
+		}
+
+		else {
+			/* 
+			missing a packet :(
+			TODO: Find better way of handling this (Maybe spin off a thread that waits for the missing frame?)
+			*/
+
+			//for now, just give up and clear the memory
+			for (size_t j = 0; j < packet_count; j++){
+				free(frame_buffer[j]);
+			}
+			free(frame_buffer);
+			return;
+		}
+
+	}
+	frame_to_decoder(frame.str());
+}
+
+// TODO: Break up into smaller functions
+// process packets and adds them to a buffer.
+void do_packet_stuff(char *&packet, char **&frame_buffer, int &packet_count){
+	#ifdef DEBUG_PACKET
+	cout << "\nPACKET[1] =" << (packet[1]>>7) << "\n";
+	#endif
+	//check if it's the last packet in the sequence
+	bool last_packet = false;
+	//if first bit of p[1] == 1
+	if ((packet[1] >> 7) == 1){
+		last_packet = true;
+		#ifdef DEBUG_PACKET
+			puts("LAST PACKET");
+			cout << packet[1] << "\n";
+		#endif
+	}
+
+
+	//get the packet's sequence number
+	//split between 4 bytes in the packet
+	//Assumes client computer isn't a dinosaur and that an int is 4 bytes
+	uint seq1 = packet[2];
+	uint seq2 = packet[3];
+	uint seq3 = packet[4];
+	uint seq4 = packet[5];	
+	//combine into one int
+	uint seq	= seq1 | (seq2 << 8) | (seq3 << 16) | (seq4 << 24);
+	
+
+
+	#ifdef DEBUG_PACKET
+		printf("Seq1: %u\nSeq2: %u\nSeq3: %u\nSeq4: %u\nSeq: %hu", seq1, seq2, seq3, seq4, seq);
+	#endif
+
+	//remove the 16 byte header, it's garbage now
+	packet += RTP_HEADER_SIZE;
+
+	
+	//add packet to frame buffer
+	frame_buffer[seq] = packet;
+	packet_count ++;
+	
+	//if last packet in sequence
+	if (last_packet){
+		//merge packets together and send it to decoder
+		merge_frame(frame_buffer, packet_count);
+		packet_count = 0;
+	}
+}
 //
 void handle_udp_packets(){
+
+	//for tracking packets
+	char **frame_buffer = (char **)malloc(sizeof(char) * BUFF_SIZE);
+	int packet_count = 0;
 
 	//for udp stream
 	SOCKET udp_sock;
@@ -239,7 +320,7 @@ void handle_udp_packets(){
 	else{
 
 		#ifdef DEBUG_PACKET
-			cout "<<\n------------------------\n" << "udp_sock bound to port " << LOCAL_PORT << "\n------------------------\n";
+			cout << "<<\n------------------------\n" << "udp_sock bound to port " << LOCAL_PORT << "\n------------------------\n";
 		#endif
 		
 	}
@@ -247,11 +328,11 @@ void handle_udp_packets(){
 	//collect packets forever
 	//will add better conditions later
 	while (true){
-		char buffer[BUFF_SIZE];
+		char *packet = (char *)malloc(sizeof(char)*BUFF_SIZE);
 		//get next packet
-		int bytes = recv(udp_sock, buffer, BUFF_SIZE, 0);
+		int bytes = recv(udp_sock, packet, BUFF_SIZE, 0);
 		
-		//bad / no backet
+		//bad or no packet
 		if (bytes < 1){
 			#ifdef DEBUG_PACKET
 				cout << "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n" << "packet not read" << "\n!!!!!!!!!!!!!!!!!!!!!!\n";
@@ -259,7 +340,7 @@ void handle_udp_packets(){
 		}
 		//good packet
 		else{
-			do_packet_stuff(buffer);
+			do_packet_stuff(packet, frame_buffer, packet_count);
 			cout << "<Packet>\n" << bytes << "\n";
 		} 
 	}
